a723e4ebe1037a4ba55930687f2bf11728410662
Bogatejc <mark.bogataj18@gmail.com>
2021-06-20 08:47:07 +0200

    Added method to clustering scripts for kcut partition on edge weights

--- scripts/clustering-utils.py
+++ scripts/clustering-utils.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     k: int = args.k
 
     # ==================================================
-    #   METIS GRAPH PARTITIONING
+    #   GRAPH PARTITIONING USING K-CUT WITH NODE WEIGHTS (number of houses connected to each crossroad)
     #   https://metis.readthedocs.io/en/latest/
     # ==================================================
     crossroad_graph = nx.Graph()
@@ -49,23 +49,23 @@ if __name__ == '__main__':
     crossroad_graph.graph['node_weight_attr'] = 'num_of_houses' 
     (cut, parts) = metis.part_graph(crossroad_graph, k)
     for i, part in enumerate(parts):
-        crossroad_graph.nodes[list(crossroad_graph.nodes)[i]]['metis_partition_id'] = str(part)
+        crossroad_graph.nodes[list(crossroad_graph.nodes)[i]]['kcut_partition_id'] = str(part)
     
     for edge in g.edges():
         node1 = edge[0]
         node2 = edge[1]
         if(node1 in crossroad_graph and node2 not in crossroad_graph):
             crossroad_graph.add_node(node2)
-            crossroad_graph.nodes[node2]['metis_partition_id'] = crossroad_graph.nodes[node1]['metis_partition_id']
+            crossroad_graph.nodes[node2]['kcut_partition_id'] = crossroad_graph.nodes[node1]['kcut_partition_id']
             crossroad_graph.add_edge(node1, node2)
         elif(node2 in crossroad_graph and node1 not in crossroad_graph):
             crossroad_graph.add_node(node1)
-            crossroad_graph.nodes[node1]['metis_partition_id'] = crossroad_graph.nodes[node2]['metis_partition_id']
+            crossroad_graph.nodes[node1]['kcut_partition_id'] = crossroad_graph.nodes[node2]['kcut_partition_id']
             crossroad_graph.add_edge(node1, node2)
 
     for n in crossroad_graph.nodes():
-        g.nodes[n]['metis_partition_id'] = str(crossroad_graph.nodes[n]['metis_partition_id'])
-    nx.write_pajek(g, f'{args.path[:-4]}-metis.net')
+        g.nodes[n]['kcut_partition_id'] = str(crossroad_graph.nodes[n]['kcut_partition_id'])
+    nx.write_pajek(g, f'{args.path[:-4]}-kcut_weighted_nodes.net')
 
     # ==================================================
     #   GIRVAN NEWMAN COMMUNITY DETECTION
@@ -102,3 +102,31 @@ if __name__ == '__main__':
         g.nodes[i]['async_fluid_cluster_id'] = str(coms.to_node_community_map()[i][0])
 
     nx.write_pajek(g, f'{args.path[:-4]}-async_fluid.net')
+
+    # ==================================================
+    #   GRAPH PARTITIONING USING K-CUT WITH EDGE WEIGHTS (distance)
+    #   https://metis.readthedocs.io/en/latest/
+    # ==================================================
+    g: nx.Graph = nx.read_pajek(args.path)
+
+    g.graph['edge_weight_attr'] = 'distance'
+    (cut, parts) = metis.part_graph(g, k)
+
+    for i, part in enumerate(parts):
+        g.nodes[list(g.nodes)[i]]['kcut_partition_id'] = str(part)
+    
+    nx.write_pajek(g, f'{args.path[:-4]}-kcut_edge_distance.net')
+
+    # ==================================================
+    #   GRAPH PARTITIONING USING K-CUT WITH EDGE WEIGHTS (duration)
+    #   https://metis.readthedocs.io/en/latest/
+    # ==================================================
+    g: nx.Graph = nx.read_pajek(args.path)
+
+    g.graph['edge_weight_attr'] = 'duration'
+    (cut, parts) = metis.part_graph(g, k)
+
+    for i, part in enumerate(parts):
+        g.nodes[list(g.nodes)[i]]['kcut_partition_id'] = str(part)
+    
+    nx.write_pajek(g, f'{args.path[:-4]}-kcut_edge_duration.net')
